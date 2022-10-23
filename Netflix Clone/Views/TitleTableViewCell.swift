5b43a26f8b4b8ba10ab2852fb260288b43e6021c
rustemmanafov <rustemmanafov1@gmail.com>
2022-10-24 02:09:51 +0400

    .

---
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix Clone
+//
+//  Created by Rustem Manafov on 24.10.22.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+   static let identifier = "TitleTableViewCell"
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlesPosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+
+}
