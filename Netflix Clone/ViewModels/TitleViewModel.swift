7462e26c8f41ec94a5504fc7c59708588f5ce67c
rustemmanafov <rustemmanafov1@gmail.com>
2022-10-24 03:28:00 +0400

    .

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Rustem Manafov on 24.10.22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    
+    let titleName: String
+    let posterURL: String
+}
