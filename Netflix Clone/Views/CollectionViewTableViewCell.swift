c27753c2991f781cb71c3c8aeb408ce532490c6b
rustemmanafov <rustemmanafov1@gmail.com>
2022-10-27 02:06:58 +0400

    .

--- Netflix Clone/Views/CollectionViewTableViewCell.swift
+++ Netflix Clone/Views/CollectionViewTableViewCell.swift
@@ -55,20 +55,17 @@ class CollectionViewTableViewCell: UITableViewCell {
         }
     }
     
-//    private func downloadTitleAt(indexPath: IndexPath) {
-//
-//
-//        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
-//            switch result {
-//            case .success():
-//                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
-//            case .failure(let error):
-//                print(error.localizedDescription)
-//            }
-//        }
-//
-//
-//    }
+    private func downloadTitleAt(indexPath: IndexPath) {
+        
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                NotificationCenter.default.post(name: NSNotification.Name("downloaded"), object: nil)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
 }
 
 
@@ -128,7 +125,7 @@ extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionVie
             identifier: nil,
             previewProvider: nil) { [weak self] _ in
                 let downloadAction = UIAction(title: "Download", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
-                    //self?.downloadTitleAt(indexPath: indexPath)
+                    self?.downloadTitleAt(indexPath: indexPath)
                 }
                 return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
             }
